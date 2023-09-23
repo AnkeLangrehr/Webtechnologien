0b0594a3b30fd3894977859e1f83834060db7913
AnkeLangrehr <anke.langrehr95@gmail.com>
2023-09-23 15:37:16 +0200

    Initial Commit -Projekte angelegt -Base Config

---
+++ webBackend/src/test/java/de/fom/webtechnologien/web/WebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+/*package de.fom.webtechnologien.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
